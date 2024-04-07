b5c11ab2b9fa71723a4e378b9f96897456ba7d0b
Matheus <matheusgranville@gmail.com>
2024-04-07 14:12:44 -0300

    completes the index page

--- main.ts
+++ main.ts
@@ -1,79 +1,195 @@
 import "./style.css";
 
-const pi = Math.PI;
-const canvas = document.querySelector("canvas");
+type Line = {
+  x: number;
+  y: number;
+  size: number;
+  blur: number;
+  radians: number;
+  displacement: number;
+  isOutOfScreen(line: Line): boolean;
+};
 
-const initialWidth = window.innerWidth;
-const initialHeight = window.innerHeight;
-
-function defineLines() {
-  const lineCount = Math.floor(Math.random() * 30);
-
-  const lines = Array.from({ length: lineCount }, () => {
-    const degrees = Math.floor(Math.random() * 360);
-    const angle = degrees * (pi / 180);
-
-    const x0 = Math.random();
-    const y0 = Math.floor(Math.random());
-
-    return {
-      x0,
-      y0,
-      angle,
-      factor: 0.2,
-    };
-  });
-
-  return lines;
+enum Quadrant {
+  First = 0,
+  Second,
+  Third,
+  Fourth,
 }
 
-const currentLines = defineLines();
+let unblurredLines = 0;
+const canvas = document.querySelector("canvas");
+const lines: Line[] = Array.from({ length: 10 }, generateLine);
 
-function drawLines() {
+function getAngleData(radians: number) {
+  const sin = Math.sin(radians);
+  const cos = Math.cos(radians);
+  const quadrant = Math.floor(radians / (Math.PI / 2)) as Quadrant;
+
+  return { sin, cos, quadrant };
+}
+
+function defineEntryPoint(
+  radians: number
+): Pick<Line, "x" | "y" | "isOutOfScreen"> {
+  const slope = Math.tan(radians);
+  const { sin, cos, quadrant } = getAngleData(radians);
+
+  const randomX = Math.floor(((Math.random() * 10) / 10) * window.innerWidth);
+  const randomY = Math.floor(((Math.random() * 10) / 10) * window.innerHeight);
+
+  const yIntercept = Math.floor(randomY - slope * randomX);
+  const xIntercept = Math.floor(-yIntercept / slope);
+
+  const widthY = Math.floor(slope * window.innerWidth + yIntercept);
+  const heightX = Math.floor((window.innerHeight - yIntercept) / slope);
+
+  const xByQuadrant = {
+    [Quadrant.First]: xIntercept,
+    [Quadrant.Second]: window.innerWidth,
+    [Quadrant.Third]: heightX,
+    [Quadrant.Fourth]: 0,
+  };
+
+  const yByQuadrant = {
+    [Quadrant.First]: 0,
+    [Quadrant.Second]: widthY,
+    [Quadrant.Third]: window.innerHeight,
+    [Quadrant.Fourth]: yIntercept,
+  };
+
+  const quandrantOutOfScreen = {
+    [Quadrant.First]: (line: Line) => {
+      const currentSize = line.displacement - line.size;
+      const sizeYProjection = cos * currentSize;
+      const sizeXProjection = sin * currentSize;
+
+      if (radians > Math.PI / 4) {
+        return sizeYProjection > window.innerHeight;
+      }
+
+      return sizeXProjection + xIntercept > window.innerWidth;
+    },
+    [Quadrant.Second]: (line: Line) => {
+      const currentSize = line.displacement - line.size;
+      const sizeYProjection = Math.abs(cos) * currentSize;
+      const sizeXProjection = sin * currentSize;
+
+      if (radians > (3 * Math.PI) / 4) {
+        return sizeXProjection > window.innerWidth;
+      }
+
+      return sizeYProjection + widthY > window.innerHeight;
+    },
+    [Quadrant.Third]: (line: Line) => {
+      const currentSize = line.displacement - line.size;
+      const sizeXProjection = Math.abs(cos) * currentSize;
+      const sizeYProjection = Math.abs(sin) * currentSize;
+
+      if (radians > (5 * Math.PI) / 4) {
+        return sizeYProjection > window.innerHeight;
+      }
+
+      return sizeXProjection > heightX;
+    },
+    [Quadrant.Fourth]: (line: Line) => {
+      const currentSize = line.displacement - line.size;
+      const sizeXProjection = cos * currentSize;
+      const sizeYProjection = Math.abs(sin) * currentSize;
+
+      if (radians > (7 * Math.PI) / 4) {
+        return sizeXProjection > window.innerWidth;
+      }
+
+      return sizeYProjection > yIntercept;
+    },
+  };
+
+  return {
+    x: xByQuadrant[quadrant],
+    y: yByQuadrant[quadrant],
+    isOutOfScreen: quandrantOutOfScreen[quadrant],
+  };
+}
+
+function generateLine(): Line {
+  const degree = Math.floor(Math.random() * 360);
+  const size = Math.floor(Math.random() * 3000 + 1000);
+
+  const radians = degree * (Math.PI / 180);
+
+  const entrypoint = defineEntryPoint(radians);
+
+  let blur = 0;
+
+  if (unblurredLines === 5) {
+    blur = Math.floor(Math.random() * 5) + 3;
+  } else {
+    unblurredLines += 1;
+  }
+
+  return {
+    size,
+    blur,
+    radians,
+    displacement: 0,
+    ...entrypoint,
+  };
+}
+
+function drawLine(line: Line, index: number) {
   const ctx = canvas?.getContext("2d");
 
-  if (!ctx || !canvas) {
+  if (!ctx) {
     return;
   }
 
-  ctx.clearRect(0, 0, initialWidth, initialHeight);
+  const { x, y, radians, size, displacement, isOutOfScreen, blur } = line;
 
-  currentLines.forEach((line) => {
-    const { x0, y0, angle } = line;
+  ctx.translate(x, y);
+  ctx.rotate(radians);
 
-    const initialX = x0 * initialWidth;
-    const initialY = y0 * initialHeight;
+  const deltaX = displacement - size;
+  const gradient = ctx.createLinearGradient(deltaX, 0, displacement, 0);
 
-    const x1 = initialWidth * Math.sin(angle) + initialX;
-    const y1 = initialHeight * Math.cos(angle) + initialY;
+  gradient.addColorStop(0, "transparent");
+  gradient.addColorStop(0.5, "black");
+  gradient.addColorStop(1, "transparent");
 
-    ctx.strokeStyle = "red";
-    ctx.lineCap = "round";
+  ctx.lineCap = "round";
+  ctx.strokeStyle = gradient;
+  ctx.filter = `blur(${blur}px)`;
 
-    ctx.beginPath();
-    ctx.moveTo(initialX, initialY);
-    ctx.lineTo(x1, y1);
-    ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(deltaX, 0);
+  ctx.lineTo(displacement, 0);
+  ctx.stroke();
+  ctx.closePath();
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
 
-    return null;
-  });
+  line.displacement += 15;
 
-  window.requestAnimationFrame(drawLines);
+  if (isOutOfScreen(line)) {
+    if (blur === 0) {
+      unblurredLines -= 1;
+    }
+
+    lines[index] = generateLine();
+  }
 }
 
-window.addEventListener("resize", () => {
+function main() {
   if (!canvas) {
     return;
   }
 
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-});
+  const scale = window.devicePixelRatio;
+  canvas.width = Math.floor(window.innerWidth * scale);
+  canvas.height = Math.floor(window.innerHeight * scale);
 
-if (canvas) {
-  canvas.width = initialWidth;
-  canvas.height = initialHeight;
-  canvas.style.aspectRatio = `auto`;
+  lines.map(drawLine);
+
+  requestAnimationFrame(main);
 }
 
-window.requestAnimationFrame(drawLines);
+requestAnimationFrame(main);
