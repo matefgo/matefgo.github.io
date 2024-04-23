bdcb25ac3617a7adcb8d80637695bd0bf1bb70c9
Matheus <matheusgranville@gmail.com>
2024-04-22 21:45:48 -0300

    new story page

--- src/story/story.ts
+++ src/story/story.ts
@@ -1,29 +1,107 @@
 import { Canvas } from "../common/classes/canvas";
 import "./style.css";
 
-type Rect = {
-  index: number;
-  speed: number;
-  isCompleted: boolean;
-  sideProgress: number[];
-};
+class Rect {
+  x: number;
+  y: number;
+  slope: number;
+  yInterceptor: number;
+  colorAngle = Math.floor(Math.random() * 360);
+  colorSpeed = Math.floor(Math.random() * 9 + 1);
+  isClockwise = Math.random() > 0.5 ? true : false;
+  glowSize = 100;
+
+  constructor(x: number, y: number, slope: number) {
+    this.x = x;
+    this.y = y;
+    this.slope = slope;
+    this.yInterceptor = y - slope * x;
+  }
+
+  getGradient(
+    pathCtx: CanvasRenderingContext2D,
+    glowCtx: CanvasRenderingContext2D,
+    dX: number,
+    dY: number
+  ) {
+    const conicGradient = pathCtx.createConicGradient(
+      ((this.colorAngle * Math.PI) / 180) * (this.isClockwise ? 1 : -1),
+      this.x,
+      this.y
+    );
+
+    if (this.isClockwise) {
+      conicGradient.addColorStop(0, "transparent");
+      conicGradient.addColorStop(1, "white");
+    } else {
+      conicGradient.addColorStop(0, "white");
+      conicGradient.addColorStop(1, "transparent");
+    }
+
+    this.colorAngle += this.colorSpeed / 10;
+
+    if (Math.round(this.colorAngle) > 360) {
+      this.drawGlow(this.colorAngle, glowCtx, dX, dY);
+    }
+
+    return conicGradient;
+  }
+
+  drawGlow(
+    currentColorAngle: number,
+    glowCtx: CanvasRenderingContext2D,
+    dX: number,
+    dY: number
+  ) {
+    glowCtx.beginPath();
+
+    glowCtx.moveTo(this.x, this.y - dY);
+    glowCtx.lineTo(this.x - dX, this.y);
+    glowCtx.lineTo(this.x, this.y + dY);
+    glowCtx.lineTo(this.x + dX, this.y);
+    glowCtx.lineTo(this.x, this.y - dY);
+
+    glowCtx.lineWidth = this.glowSize / 5;
+    glowCtx.strokeStyle = "#003FFF";
+    glowCtx.stroke();
+
+    glowCtx.closePath();
+
+    this.glowSize -= 1;
+
+    if (this.glowSize === 0) {
+      this.colorAngle = currentColorAngle % 360;
+      this.glowSize = 100;
+    }
+  }
+}
 
 export class Story {
-  yDisplacement = 0;
   currentAnimationId = 0;
 
   glowsCanvas: Canvas;
   pathsCanvas: Canvas;
 
-  dX = 0;
-  dY = 0;
+  angle: number;
+  size: number;
+  initialProgress = 0;
+
+  dX: number;
+  dY: number;
 
   rects: Rect[];
-  glows: any[] = [];
 
   constructor() {
     this.glowsCanvas = new Canvas("#glows");
     this.pathsCanvas = new Canvas("#paths");
+
+    this.angle = 130;
+    this.size = Math.floor(this.pathsCanvas.width / 3);
+
+    const radians = (this.angle * Math.PI) / 180;
+
+    this.dX = this.size * Math.sin(radians / 2);
+    this.dY = this.size * Math.cos(radians / 2);
 
     this.rects = this.buildRects();
 
@@ -31,32 +109,51 @@ export class Story {
   }
 
   buildRects() {
-    const angle = 130;
-    const size = window.innerWidth / 3;
-
-    const radians = (angle * Math.PI) / 180;
-
-    this.dX = size * Math.sin(radians / 2);
-    this.dY = size * Math.cos(radians / 2);
+    const finalX = this.pathsCanvas.width / 2;
 
     return Array.from({ length: 10 }, (_, index) => {
-      const rect = {
-        index,
-        isCompleted: false,
-        sideProgress: [0, 0, 0, 0],
-        speed: Math.floor(Math.random() * 7 + 3) / 10,
-      };
+      const xSign = Math.random() > 0.5 ? 1 : -1;
+      const tanSign = Math.random() > 0.5 ? 1 : -1;
+      const tan = tanSign * Math.tan(Math.PI / 6);
 
-      return rect;
+      const finalY = ((index + 1) / 10) * this.pathsCanvas.heigth;
+      const yInterceptor = finalY - tan * finalX;
+
+      const widthFactor = xSign > 0 ? this.pathsCanvas.width : 0;
+
+      const initialX = Math.floor(xSign * this.dX + widthFactor);
+      const initialY = Math.floor(tan * initialX + yInterceptor);
+
+      return new Rect(initialX, initialY, tan);
     });
   }
 
-  drawRectPath(rect: Rect) {
-    const { index, sideProgress, speed } = rect;
+  drawRects(rect: Rect) {
+    const { x, y } = rect;
 
-    const x = innerWidth / 2;
-    const y = 3 * this.dY * index;
+    this.pathsCanvas.ctx.beginPath();
 
+    this.pathsCanvas.ctx.moveTo(x, y - this.dY);
+    this.pathsCanvas.ctx.lineTo(x - this.dX, y);
+    this.pathsCanvas.ctx.lineTo(x, y + this.dY);
+    this.pathsCanvas.ctx.lineTo(x + this.dX, y);
+    this.pathsCanvas.ctx.lineTo(x, y - this.dY);
+
+    const gradient = rect.getGradient(
+      this.pathsCanvas.ctx,
+      this.glowsCanvas.ctx,
+      this.dX,
+      this.dY
+    );
+
+    this.pathsCanvas.ctx.lineWidth = 2;
+    this.pathsCanvas.ctx.strokeStyle = gradient;
+    this.pathsCanvas.ctx.stroke();
+
+    this.pathsCanvas.ctx.closePath();
+  }
+
+  drawScene() {
     this.pathsCanvas.ctx.clearRect(
       0,
       0,
@@ -64,134 +161,6 @@ export class Story {
       this.pathsCanvas.heigth
     );
 
-    if (sideProgress[0] < 100) {
-      this.pathsCanvas.ctx.moveTo(x, y - this.dY);
-      this.pathsCanvas.ctx.lineTo(
-        x - (this.dX * sideProgress[0]) / 100,
-        y - this.dY + (this.dY * sideProgress[0]) / 100
-      );
-
-      sideProgress[0] += speed;
-    }
-
-    if (sideProgress[0] >= 100 && sideProgress[1] < 100) {
-      this.pathsCanvas.ctx.moveTo(x, y - this.dY);
-      this.pathsCanvas.ctx.lineTo(x - this.dX, y);
-      this.pathsCanvas.ctx.lineTo(
-        x - this.dX + (this.dX * sideProgress[1]) / 100,
-        y + (this.dY * sideProgress[1]) / 100
-      );
-
-      sideProgress[1] += speed;
-    }
-
-    if (sideProgress[1] >= 100 && sideProgress[2] < 100) {
-      this.pathsCanvas.ctx.moveTo(x, y - this.dY);
-      this.pathsCanvas.ctx.lineTo(x - this.dX, y);
-      this.pathsCanvas.ctx.lineTo(x, y + this.dY);
-      this.pathsCanvas.ctx.lineTo(
-        x + (this.dX * sideProgress[2]) / 100,
-        y + this.dY - (this.dY * sideProgress[2]) / 100
-      );
-
-      sideProgress[2] += speed;
-    }
-
-    if (sideProgress[2] >= 100 && sideProgress[3] < 100) {
-      this.pathsCanvas.ctx.moveTo(x, y - this.dY);
-      this.pathsCanvas.ctx.lineTo(x - this.dX, y);
-      this.pathsCanvas.ctx.lineTo(x, y + this.dY);
-      this.pathsCanvas.ctx.lineTo(x + this.dX, y);
-      this.pathsCanvas.ctx.lineTo(
-        x + this.dX - (this.dX * sideProgress[3]) / 100,
-        y - (this.dY * sideProgress[3]) / 100
-      );
-
-      sideProgress[3] += speed;
-    }
-
-    if (sideProgress.every((progress) => progress >= 100)) {
-      if (!rect.isCompleted) {
-        rect.isCompleted = true;
-      }
-
-      const rectGlow = this.glows.find((glow) => glow.id === index);
-
-      this.pathsCanvas.ctx.moveTo(x, y - this.dY);
-      this.pathsCanvas.ctx.lineTo(x - this.dX, y);
-      this.pathsCanvas.ctx.lineTo(x, y + this.dY);
-      this.pathsCanvas.ctx.lineTo(x + this.dX, y);
-      this.pathsCanvas.ctx.lineTo(x, y - this.dY);
-
-      if (!rectGlow) {
-        this.glows.push({
-          id: index,
-          y,
-          size: 100,
-          oscilator: 0,
-        });
-      }
-    }
-  }
-
-  drawRects() {
-    this.pathsCanvas.ctx.beginPath();
-
-    if (this.rects.every((rect) => rect.isCompleted)) {
-      this.yDisplacement += 1;
-
-      if (this.yDisplacement > innerHeight) {
-        this.yDisplacement = 0;
-      }
-    }
-
-    for (let rect of this.rects) {
-      this.drawRectPath(rect);
-    }
-
-    this.pathsCanvas.ctx.strokeStyle = "white";
-    this.pathsCanvas.ctx.lineWidth = 2;
-    this.pathsCanvas.ctx.stroke();
-  }
-
-  drawGlowPath(glow: any) {
-    const { y } = glow;
-
-    const x = innerWidth / 2;
-
-    this.glowsCanvas.ctx.save();
-
-    this.glowsCanvas.ctx.beginPath();
-
-    this.glowsCanvas.ctx.moveTo(x, y - this.dY);
-    this.glowsCanvas.ctx.lineTo(x - this.dX, y);
-    this.glowsCanvas.ctx.lineTo(x, y + this.dY);
-    this.glowsCanvas.ctx.lineTo(x + this.dX, y);
-    this.glowsCanvas.ctx.lineTo(x, y - this.dY);
-
-    this.glowsCanvas.ctx.closePath();
-
-    this.glowsCanvas.ctx.strokeStyle = "#0059ff";
-    this.glowsCanvas.ctx.lineWidth = glow.size / 5;
-
-    if (glow.size > 50) {
-      glow.size -= 1;
-    } else {
-      glow.size = 25 * Math.cos((glow.oscilator * Math.PI) / 180) + 25;
-
-      glow.oscilator += 1;
-
-      if (glow.oscilator === 360) {
-        glow.oscilator = 0;
-      }
-    }
-
-    this.glowsCanvas.ctx.stroke();
-
-    this.glowsCanvas.ctx.restore();
-  }
-
-  drawGlows() {
     this.glowsCanvas.ctx.clearRect(
       0,
       0,
@@ -199,15 +168,23 @@ export class Story {
       this.glowsCanvas.heigth
     );
 
-    for (let rect of this.glows) {
-      this.drawGlowPath(rect);
-    }
-  }
+    this.rects.map((rect) => {
+      this.drawRects(rect);
+      const canvasWidth = this.pathsCanvas.width;
 
-  drawScene() {
-    this.drawRects();
+      if (Math.round(this.initialProgress) === 500) {
+        return null;
+      }
 
-    this.drawGlows();
+      const distance = canvasWidth / 2 - rect.x;
+
+      rect.x += Math.floor((distance * this.initialProgress) / 500);
+      rect.y = Math.floor(rect.yInterceptor + rect.slope * rect.x);
+
+      this.initialProgress += 1;
+
+      return null;
+    });
 
     this.animateCanvas();
   }
